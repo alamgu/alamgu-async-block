3e4c53b8d84cfe513c8912bc1739388836d97054
Jonathan D.K. Gibbons <jonored@gmail.com>
2022-12-29 22:27:53 +0000

    Remove unneeded trace statements.

--- src/prompts.rs
+++ src/prompts.rs
@@ -53,15 +53,11 @@ impl PromptQueue {
     }
 
     pub async fn show(&mut self) -> Result<bool, PromptingError> {
-        trace!("Attempting to prompt, {:?}", self);
         if self.prev == [0; 32] { return Err(PromptingError); } // No showing empty PromptQueues.
         
         let mut forward = self.reverse().await?;
-        trace!("Reversed");
         let mut backward = Self::new(self.io);
-        trace!("Backward crated");
         let mut title_and_body = forward.pop().await?.expect("already checked for this?");
-        trace!("Got first page, {}, {}", title_and_body.0, title_and_body.1);
 
         #[derive(Clone, Copy, Debug)]
         enum PromptingState {
@@ -74,11 +70,9 @@ impl PromptQueue {
 
         loop {
             // Display
-            trace!("Looping on prompts");
             let (currentTitle, currentBody) = title_and_body;
             match state {
                 PromptingState::Prompts => {
-                    trace!("Showing prompts");
                     Bagl::LABELLINE(LabelLine::new().pos(0, 10).text(currentTitle.as_str())).display();
                     #[cfg(target_os = "nanos")]
                         {
@@ -105,14 +99,12 @@ impl PromptQueue {
                     RIGHT_ARROW.paint();
                 }
                 PromptingState::Confirm => {
-                    trace!("Showing confirm");
                     Bagl::ICON(Icon::new(Icons::CheckBadge).pos(18,12)).display();
                     Bagl::LABELLINE(LabelLine::new().text("Confirm").pos(0, 20)).paint();
                     LEFT_ARROW.paint();
                     RIGHT_ARROW.paint();
                 }
                 PromptingState::Cancel => {
-                    trace!("Showing cancel");
                     Bagl::ICON(Icon::new(Icons::CrossBadge).pos(18,12)).display();
                     Bagl::LABELLINE(LabelLine::new().text("Cancel").pos(0, 20)).paint();
                     LEFT_ARROW.paint();
@@ -124,30 +116,22 @@ impl PromptQueue {
                 _ => { }
             } }; */
 
-            trace!("Getting next button event");
             // let buttons_evt = self.io.next_button().await;
-            trace!("Post button state: {:?}", self);
             loop {
             if let Some(buttons_evt) = nanos_ui::ui::get_event(&mut buttons) {
             // {
-                // trace!("Received buttons {:?}", (state.clone(), buttons_evt));
                 match (state.clone(), buttons_evt) {
                     (PromptingState::Prompts, ButtonEvent::LeftButtonRelease) => {
-                        trace!("Backward");
                         if backward.prev != [0; 32] {
                             forward.add_prompt_chunk(&currentTitle, &currentBody).await?;
                             title_and_body = backward.pop().await?.unwrap();
                         }
                     }
                     (PromptingState::Prompts, ButtonEvent::RightButtonRelease) => {
-                        trace!("Forward");
                         if forward.prev != [0; 32] {
-                            trace!("Adding current prompt page to reverse queue");
                             backward.add_prompt_chunk(&currentTitle, &currentBody).await?;
-                            trace!("Getting next prompt page");
                             title_and_body = forward.pop().await?.unwrap();
                         } else {
-                            trace!("Prompts show, going to confirm");
                             state = PromptingState::Confirm;
                         }
                     }
@@ -168,10 +152,8 @@ impl PromptQueue {
                     }
                     _ => { }
                 }
-                trace!("Buttons handled");
                 break;
             } else {
-                // trace!("Some other event");
                 }
             }
         }
@@ -180,34 +162,23 @@ impl PromptQueue {
     async fn add_prompt_chunk(&mut self, title: &str, segment: &str) -> Result<(), PromptingError> {
         if title.len() > 17 { return Err(PromptingError); }
         if segment.len() > PROMPT_CHUNK_LENGTH { return Err(PromptingError); }
-        // trace!("Adding prompt chunk: {}, {}", title, segment);
         let mut chunk = PromptBuffer::new();
         chunk.try_extend_from_slice(&self.prev);
         chunk.push(title.len() as u8);
         chunk.push(segment.len() as u8);
-        // trace!("CHUNK: {:?}", chunk);
         chunk.try_extend_from_slice(title.as_bytes()); // .unwrap();
-        // trace!("CHUNK: {:?}", chunk);
         chunk.try_extend_from_slice(segment.as_bytes()); // .unwrap();
-        // trace!("CHUNK: {:?}", chunk);
-        trace!("Buffer is: {:?}, segment: {}", chunk, segment);
         self.prev = self.io.put_chunk(&chunk).await;
-        trace!("Chunk put.");
         Ok(())
     }
 
     pub async fn add_prompt(&mut self, title: &str, args: Arguments<'_>) -> Result<(), PromptingError> {
-        trace!("Add prompt");
         let mut writer = ChunkedWrite::new();
         while !writer.terminated() {
-            trace!("Writing");
             core::fmt::write(&mut writer, args);
-            trace!("Adding chunk, state: {:?}", &writer);
             self.add_prompt_chunk(title, writer.get_buffer()).await?;
             writer.advance();
-            trace!("Advanced.");
         }
-        trace!("Prompt saved");
         Ok(())
     }
 }
@@ -238,7 +209,6 @@ impl ChunkedWrite {
         self.skip += PROMPT_CHUNK_LENGTH;
         self.skip_this_pass = self.skip;
         self.len = 0;
-        trace!("Advanced: {:?}", self)
     }
     fn terminated(&self) -> bool {
         self.terminated
@@ -254,20 +224,15 @@ impl Write for ChunkedWrite {
         
         if self.skip_this_pass > s.len() {
             self.skip_this_pass -= s.len();
-            trace!("Skipping still");
             return Ok(());
         }
         if self.buffer.is_full() {
-            trace!("Full");
             return Ok(());
         }
         let pushed_slice = &s[self.skip_this_pass .. core::cmp::min(s.len(), PROMPT_CHUNK_LENGTH + self.skip_this_pass - self.buffer.len())];
-        trace!("Push slice: {}", pushed_slice);
         self.skip_this_pass = core::cmp::max(0, self.skip_this_pass as isize - s.len() as isize) as usize;
-        trace!("Updated skip, pushing");
 
         self.buffer.try_push_str(pushed_slice);
-        trace!("Pushed to buffer");
         Ok(())
     }
 }
